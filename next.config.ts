ac3e25ebf3cbc830b7c0d11fd5e468add64ffab0
balazs.saros <balazs.saros@gmail.com>
2025-06-13 21:21:01 +0200

    deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'build',
+  output: 'export',
+  basePath: '/obunda'
 };
 
 export default nextConfig;
